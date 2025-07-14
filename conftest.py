308ea1f86a067889494988d5c9f255f0cf967bb8
somanytearsss <somanytearsss@yandex.ru>
2025-07-14 19:32:13 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+import requests
+
+from data import Urls
+from helpers import generate_data_and_create_user
+
+
+@pytest.fixture
+def registration_and_delete_user():
+    payload, response = generate_data_and_create_user()
+    yield payload, response
+    access_token = response.json().get('accessToken')
+    requests.delete(Urls.url_delete_user, headers={'Authorization': access_token})
